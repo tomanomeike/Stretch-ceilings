41c94cf81ccdffd3621cddd206fdfa6dcd702e62
LAPTOP-D77I3KMJ\toman <tomanom@gmail.com>
2019-03-18 19:23:21 +0200

    start

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer class="page-footer font-small">
+    <div class="footer-copyright text-center py-3">© 2019 Dariaus lubos</div>
+
+</footer>
+
+</body>
+<script src="js/jquery.js"></script>
+<script src="js/js.js"></script>
+</html>
---
+++ header.php
@@ -0,0 +1,22 @@
+<meta charset="UTF-8">
+<meta name="viewport"
+      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.1.3/darkly/bootstrap.min.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+<link rel="stylesheet" href="css/jquery.datetimepicker.css"/ >
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+<title>Dariaus lubos</title>
+</head>
+<body>
+<header>
+    <nav class="navbar">
+        <a class="navbar-brand" href="#">
+            <img src="img/logo.png" width="70" height="70">
+        </a>
+        <a href = "#">Apie mus</a>
+        <a href = "#">Galerija</a>
+        <a href = "#">Kontaktai</a>
+    </nav>
+</header>
